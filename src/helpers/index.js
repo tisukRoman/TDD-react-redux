8a6ef79a8a60a1b15259fe2c2dd9ea8abef828e6
tisukRoman <tisukroman@gmail.com>
2022-02-21 18:02:03 +0200

    getLetterMatchCount helper function added

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,5 @@
-export const getLetterMatchCount = (guessedWord, correctWord) => {
-
+export const getLetterMatchCount = (guessedWord, secretWord) => {
+    const secretWordLetters = new Set(secretWord.split(''));
+    const guessedWordLetters = new Set(guessedWord.split(''));
+    return [...secretWordLetters].filter(l => guessedWordLetters.has(l)).length;
 }
